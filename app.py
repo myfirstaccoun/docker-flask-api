8cad7ea81b5d8d04fe5e5dc57ce465ca1837f046
myfirstaccoun <85413282+myfirstaccoun@users.noreply.github.com>
2025-08-09 23:42:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+import subprocess
+
 app = Flask(__name__)
- 
+
 @app.route('/')
 def hello_world():
-    return 'Hello from Koyeb'
- 
- 
+    try:
+        # شغّل أمر ffmpeg -version
+        result = subprocess.run(
+            ["ffmpeg", "-version"],
+            capture_output=True,
+            text=True
+        )
+        output = result.stdout.split("\n")[0]  # أول سطر فيه الإصدار
+    except FileNotFoundError:
+        output = "ffmpeg not found"
+
+    return f"Hello from Koyeb<br>FFmpeg status: {output}"
+
 if __name__ == "__main__":
     app.run()
